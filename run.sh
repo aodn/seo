31ec8107bd032f7b3216e497f79aad678a77eaee
Dan Fruehauf <malkodan@gmail.com>
2015-01-22 17:00:21 +1100

    tidying run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 set -x
 
 rm output* -rf 2> /dev/null
 
-java -jar lib/saxon9he.jar  src/dummy.xml src/records-process.xsl
+java -jar lib/saxon9he.jar src/dummy.xml src/records-process.xsl
 cp src/imos.css output
 tar czf output.tgz output/
