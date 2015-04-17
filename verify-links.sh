ec08ff4c0a594733ca1b56402d046142ae24d533
Dan Fruehauf <malkodan@gmail.com>
2015-04-17 13:05:00 +1000

    a utility to verify generated links

---
+++ verify-links.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# a little utility to verify generated SEO links
+#
+# in order to grab the broken links after execution, run:
+# grep -v " 200$" output_file | cut -d' ' -f1
+#
+
+declare -r BASE_URL="https://imos.aodn.org.au"
+
+# main
+main() {
+    tmp_links=`mktemp`
+    curl -s "$base_url/data_collections.html" | grep "<h3><a href" | cut -d'"' -f2 > $tmp_links && \
+    for link in `cat $tmp_links`; do
+        echo -n "$link "
+        curl -s -w "%{http_code}\n" -o /dev/null "$base_url/$link"
+    done
+    rm -f $tmp_links
+}
+
+main "$@"
